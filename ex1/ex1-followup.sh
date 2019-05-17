01512b41de2d2bfdf6640b0edf8345258d71b557
Natan Rodrigues de Oliveira <natanrodriguesdeoliveira@gmail.com>
2019-05-16 22:52:15 -0300

    Update ex1-followup.sh

--- ex1/ex1-followup.sh
+++ ex1/ex1-followup.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 g++ ex1-followup.cpp -o ex1-followup.o -std=c++11 -O2
-./ex1-followup.o < ex1.in > ex1-followup-res.out
-diff -s ex1-followup-res.out ex1-followup.out
+./ex1-followup.o < ex1.in > ex1-followup.res
+diff -s ex1-followup.res ex1-followup.out
