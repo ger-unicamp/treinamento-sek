bc26a8eaf04f052fae78fc491422f83dd293575e
Natan Rodrigues de Oliveira <natanrodriguesdeoliveira@gmail.com>
2019-05-16 23:56:30 -0300

    Update ex3.sh

--- ex3/ex3.sh
+++ ex3/ex3.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
 g++ ex3.cpp -o ex3.o -std=c++11 -O2
 ./ex3.o < ex3.in > ex3.res
-diff -s ex3.res ex3.out
+cmp -s ex3.res ex3.out
+if [ $? -eq 0 ];
+then echo "Correto"
+else
+  echo "Incorreto"
+  diff ex3.res ex3.out
+fi
