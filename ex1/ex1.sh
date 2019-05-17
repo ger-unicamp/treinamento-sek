0af6f398b7315a07fe00e5058ed30391034f25d5
Natan Rodrigues de Oliveira <natanrodriguesdeoliveira@gmail.com>
2019-05-16 23:53:01 -0300

    Update ex1.sh

--- ex1/ex1.sh
+++ ex1/ex1.sh
@@ -1,4 +1,10 @@
 #!/bin/sh
 g++ ex1.cpp -o ex1.o -std=c++11 -O2
 ./ex1.o < ex1.in > ex1.res
-diff -s ex1.out ex1.res
+cmp -s ex1.out ex1.res
+if [ $? -eq 0 ];
+then echo "Correto"
+else
+  echo "Incorreto"
+  diff ex1.out ex1.res
+fi
