637c5446ce0e5520659940095fa010e9d9daf63c
Natan Rodrigues de Oliveira <natanrodriguesdeoliveira@gmail.com>
2019-05-16 23:55:17 -0300

    Update ex2.sh

--- ex2/ex2.sh
+++ ex2/ex2.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
 g++ ex2.cpp -o ex2.o -std=c++11 -O2
-./ex2.o < ex2.in > ex2-res.out
-diff -s ex2.out ex2-res.out
+./ex2.o < ex2.in > ex2.res
+cmp -s ex2.out ex2.res
+if [ $? -eq 0 ];
+then echo "Correto"
+else
+  echo "Incorreto"
+  diff ex2.res ex2.out
+fi
